bf57bccfa6c31e1527553777c91924136ba0fc93
gh-actions <actions@github.com>
2020-07-15 22:46:28 +0000

    Deploy website - based on b8838bab740ce27e6dfd6f2a55f5252520f2af45

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("remark-admonitions/styles/infima.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("infima/dist/css/default/default.css"),
-  require("/Users/pinea/wordweaver-docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/pinea/wordweaver-docs/src/css/custom.css"),
-];
